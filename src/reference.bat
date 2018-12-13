9b726dfb6880bf5e219e39928cc8afb3af9dd736
IGUICHI IMAD <imad.iguichi@alumni.univ-avignon.fr>
2018-12-13 17:09:26 +0100

    axtraire references

---
+++ src/reference.bat
@@ -0,0 +1,20 @@
+
+
+
+
+
+cd "/home/nas02a/etudiants/inf/uapv1900103/Donnees_itinerantes/Bureau/Papers"
+mkdir text
+mkdir info
+mkdir information
+for i in *.pdf
+do
+echo 
+pdftotext -layout -raw "" "text/.txt"
+sed -n '/References/,/aaa/P' "text/.txt" >> "info/.txt"
+sed -n '/REFERENCES/,/aaa/P' "text/.txt" >> "info/.txt"
+
+
+ 
+done
+eof
